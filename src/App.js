0ae09cadc50c971d12dbae52be70a0e5f1f3adcb
Maxi-SR <schmidtmaxi46@gmail.com>
2023-02-14 12:09:06 -0300

    sign up component and add firebase auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
+import SignUp from "./pages/SignUp";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/account" element={<Account/>} />
       </Routes>
+    </AuthContextProvider>
     </>
   );
 }
