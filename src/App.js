71ac28bd30407ffef7bc9f0a79f3b8705a988f0b
Maxi-SR <schmidtmaxi46@gmail.com>
2023-02-14 12:57:17 -0300

    login functionality done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { AuthContextProvider } from "./context/AuthContext";
 import SignUp from "./pages/SignUp";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -15,7 +16,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<SignUp/>} />
-        <Route path="/account" element={<Account/>} />
+        <Route path="/account" element={
+          <ProtectedRoute>
+            <Account/>
+          </ProtectedRoute>
+        } />
       </Routes>
     </AuthContextProvider>
     </>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
+
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const {user, signUp} = UserAuth();
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await signUp(email, password);
+            navigate('/');
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
   return (
     <>
         <div className='w-full h-screen'>
@@ -13,9 +30,9 @@ const SignUp = () => {
                 <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white '>
                     <div className='max-w-[320px] mx-auto py-16'>
                         <h1 className='text-3xl font-bold '>Sign Up</h1>
-                        <form className='w-full flex flex-col py-4'>
-                            <input className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder='Email' autoComplete='email' />
-                            <input className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder='Password' autoComplete='current-password'/>
+                        <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
+                            <input onChange={(e) => setEmail(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder='Email' autoComplete='email' />
+                            <input onChange={(e) => setPassword(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder='Password' autoComplete='current-password'/>
                             <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
                         <div className='flex justify-between items-center text-sm text-gray-600'>
                             <p><input type="checkbox" className='mr-2' />Remember me</p>
